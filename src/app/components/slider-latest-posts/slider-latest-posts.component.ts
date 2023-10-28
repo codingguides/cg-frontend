21062a42731023aa82a0b1ac3618782cf39e888f
codingguides <info.codingguides@gmail.com>
2023-10-28 12:57:59 +0530

    Owl carousel update

--- src/app/components/slider-latest-posts/slider-latest-posts.component.ts
+++ src/app/components/slider-latest-posts/slider-latest-posts.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CommonService } from '../../common/common.service';
+import { ActivatedRoute, Router } from '@angular/router';
+declare var jQuery: any;
 
 @Component({
   selector: 'app-slider-latest-posts',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./slider-latest-posts.component.css']
 })
 export class SliderLatestPostsComponent {
+
+  constructor(
+    public commonservice: CommonService,
+    private router: ActivatedRoute,
+    private routerurl: Router
+  ) {
+    this.getBlog();
+  }
+
+  blog: any;
+  showBlog:boolean = false;
+
+  // ngOnInit() {
+  //   this.getBlog();
+  // }
+
+  async getBlog() {
+    await this.commonservice
+      .get(`page/blog`)
+      .subscribe((result) => {
+        const apiResult = JSON.parse(JSON.stringify(result));
+        if (apiResult && apiResult.status == 'SUCCESS') {
+          this.blog = apiResult && apiResult.payload;
+          console.log("this.blog>>>>>>>>>>",this.blog)
+          this.showBlog = true;
+        }else{
+          this.showBlog = false;
+        }
+      });
+  }
 
 }
