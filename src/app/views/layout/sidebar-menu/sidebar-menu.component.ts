12ec37fd4fcee0290e2b3d471055047dd863ae60
avtarSuman95 <naskarsuman195@gmail.com>
2024-01-04 20:28:26 +0530

    update sharedService in sidebarmenu

--- src/app/views/layout/sidebar-menu/sidebar-menu.component.ts
+++ src/app/views/layout/sidebar-menu/sidebar-menu.component.ts
@@ -7,7 +7,7 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-sidebar-menu',
   templateUrl: './sidebar-menu.component.html',
-  styleUrls: ['./sidebar-menu.component.css']
+  styleUrls: ['./sidebar-menu.component.css'],
 })
 export class SidebarMenuComponent {
   quizInterruptStatus: any;
@@ -18,7 +18,7 @@ export class SidebarMenuComponent {
     public sharedService: SharedService,
     private router: ActivatedRoute,
     private routerurl: Router
-  ) { }
+  ) {}
 
   menu_sidebar: any;
   param: any;
@@ -28,25 +28,31 @@ export class SidebarMenuComponent {
   ngOnInit() {
     this.getMenuSidebar();
 
-    this.quizInterruptStatus = this.commonservice.castQuizStatus.subscribe((obj: any) => {
-      console.log(obj.status, "aaaaa", obj.currentUrl, "bbbbbbb", obj.userId)
-      this.quizStatus = obj.status;
-      this.quizCurrentUrl = obj.currentUrl;
-    })
+    this.quizInterruptStatus = this.commonservice.castQuizStatus.subscribe(
+      (obj: any) => {
+        console.log(obj.status, 'aaaaa', obj.currentUrl, 'bbbbbbb', obj.userId);
+        this.quizStatus = obj.status;
+        this.quizCurrentUrl = obj.currentUrl;
+      }
+    );
   }
   async getMenuSidebar() {
-
     this.param = this.router.snapshot.params['topic'];
     this.subparam = this.router.snapshot.params['subtopic'];
     this.selectedquiz = this.router.snapshot.params['quiz'];
-    console.log("this.router.snapshot.params>>>>>>", this.router.snapshot.params)
+    console.log(
+      'this.router.snapshot.params>>>>>>',
+      this.router.snapshot.params
+    );
     await this.commonservice
       .get(`page/get-sidebar-menu`)
       .subscribe((result) => {
         const apiResult2 = JSON.parse(JSON.stringify(result));
 
         if (apiResult2 && apiResult2.status == 'SUCCESS') {
-          this.menu_sidebar = apiResult2 && apiResult2.payload.find((menu: any) => menu.slug == this.param);
+          this.menu_sidebar =
+            apiResult2 &&
+            apiResult2.payload.find((menu: any) => menu.slug == this.param);
           this.menu_sidebar = this.menu_sidebar.children;
         }
       });
@@ -54,28 +60,26 @@ export class SidebarMenuComponent {
 
   navigate(parentpath: any, path: any) {
     this.commonservice.selectedTopicID = path._id;
-    console.log("IDDDD........", this.commonservice.selectedTopicID)
-    localStorage.setItem('topic_id', path._id)
+    console.log('IDDDD........', this.commonservice.selectedTopicID);
+    localStorage.setItem('topic_id', path._id);
     let url = '';
-    console.log("Parentpath>>>>>>>", parentpath)
-    console.log("Param>>>>>>>>", this.param)
-    console.log("URL>>>>>>>>", url)
-    console.log("Path>>>>>>>", path)
+    console.log('Parentpath>>>>>>>', parentpath);
+    console.log('Param>>>>>>>>', this.param);
+    console.log('URL>>>>>>>>', url);
+    console.log('Path>>>>>>>', path);
 
     if (parentpath == this.param) {
-      url = 'quiz/' + parentpath + '/' + path
-      console.log("If url...........", url)
-    }
-    else if (parentpath !== this.param) {
-      url = 'quiz/' + this.param + '/' + parentpath.slug + '/' + path.slug
-      console.log("Else url.........", url)
+      url = 'quiz/' + parentpath + '/' + path;
+      console.log('If url...........', url);
+    } else if (parentpath !== this.param) {
+      url = 'quiz/' + this.param + '/' + parentpath.slug + '/' + path.slug;
+      console.log('Else url.........', url);
     }
 
-    // this.sharedService.isQuizLiveCheck(url, true);
+    this.sharedService.isQuizLiveCheck(url, true);
   }
 
   capitalizeFirstLetter(string: string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
-
 }
